bbeb2378993d8739da2d8488ae54bcabf699af79
gaurmahi12 <gaurmahi12@gmail.com>
2025-06-05 20:54:02 +0000

    Added DataTypes Naming Variables in JS

---
+++ datatypedemo.js
@@ -0,0 +1,7 @@
+let productprice =150000;
+
+let productdiscount = 15;
+
+let Productname = "Apple iphone 11 Pro max (512 GB, Blue)"
+
+let isAvailable =true;
---
+++ variablename.js
@@ -0,0 +1,8 @@
+let first+name= 10;
+
+//as you cab see the compiler will through syntax error because this code line 
+//is not following the rules for executing a name of variable.
+
+let first$name ="Gega-Byte"
+
+//where this is following correct syntax
